25e14cef8b2daa1d73219540ab3f5a19317df6f7
Devini Munasinghe <devinimadumali@gmail.com>
2025-07-12 09:04:18 +0530

    update item routes

--- api/routes/item-routes.js
+++ api/routes/item-routes.js
@@ -10,8 +10,8 @@ const upload = multer({ storage });
 // Upload a new item (admin only)
 router.post(
   "/add-item",
-  protect,
-  adminOrSuperAdmin,
+  //protect,
+  //adminOrSuperAdmin,
   upload.single("item_image"),
   itemController.addItem
 );
@@ -20,25 +20,30 @@ router.post(
 router.get("/category/:category_name", itemController.getItemsByCategory);
 
 // Fetch all items
-router.get("/get_all", protect, adminOrSuperAdmin, itemController.getAllItems);
+router.get("/get_all", 
+  //protect, 
+  //adminOrSuperAdmin, 
+  itemController.getAllItems);
 
 // Fetch a single item by its ID
-router.get("/:item_id", protect, itemController.getItemById);
+router.get("/:item_id", 
+  //protect, 
+  itemController.getItemById);
 
 // Update an item (with image upload)
 router.put(
   "/:item_id",
   upload.single("item_image"),
-  protect,
-  adminOrSuperAdmin,
+  //protect,
+  //adminOrSuperAdmin,
   itemController.updateItem
 );
 
 // Delete an item (soft delete)
 router.delete(
   "/:item_id",
-  protect,
-  adminOrSuperAdmin,
+  //protect,
+  //adminOrSuperAdmin,
   itemController.deleteItem
 );
 
